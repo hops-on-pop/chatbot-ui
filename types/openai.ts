cf1e00dc513f484fee23ed09865af5c42fb7f778
Mike Jones <mikejone@usc.edu>
2024-03-09 16:51:45 -0800

    update gpt-4 model

--- types/openai.ts
+++ types/openai.ts
@@ -10,12 +10,12 @@ export interface OpenAIModel {
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
   GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_4 = 'gpt-4',
+  GPT_4 = 'gpt-4-turbo-preview',
   GPT_4_32K = 'gpt-4-32k',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.GPT_4;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
